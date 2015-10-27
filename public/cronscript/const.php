d01a4671d85f567981680d10669f50c403504c16
businessworld@2015 <godigital@businessworld.in>
2015-10-27 17:13:33 +0530

    config changes

--- public/cronscript/const.php
+++ public/cronscript/const.php
@@ -1,12 +1,2 @@
 <?php
-date_default_timezone_set('Asia/Calcutta');
-define('HOST','cmsdb.cfdluvagb8xv.ap-southeast-1.rds.amazonaws.com');
-define('USER','bwcms');
-define('PASS','bwpassword2015');
-define('DATABASE','bwcms');
 
-
-define('LHOST','websitedb.cfdluvagb8xv.ap-southeast-1.rds.amazonaws.com');
-define('LUSER','bwwebsite');
-define('LPASS','bwwebpass');
-define('LDATABASE','websitedb');
