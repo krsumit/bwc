4eeb3cb80cb3901db6a20f75cea873328cfe903f
businessworld@2015 <godigital@businessworld.in>
2015-11-09 18:16:10 +0530

    maijob changes

--- public/mailscript/mailjob.php
+++ public/mailscript/mailjob.php
@@ -1,8 +1,9 @@
+#!/usr/bin/php -q
 <?php 
 session_start();
 error_reporting(E_ALL);
-//parse_str($argv['1']);      // Uncommment this line if you are hittig from url cli , It's being used in cron setting amazonee		
-$section=$_GET['section'];    // Uncommment this line if you are hittig from url
+parse_str($argv['1']);      // Uncommment this line if you are hittig from url cli , It's being used in cron setting amazonee		
+//$section=$_GET['section'];    // Uncommment this line if you are hittig from url
 include 'mail.php';
 $cronobj = new Cron();
 $cronobj->sendMailData($section);
