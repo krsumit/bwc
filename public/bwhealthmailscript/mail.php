cf6bbb97fb9205a808f1a2182451c8abe032f49b
root <godigital@businessworld.in>
2019-06-04 18:41:41 +0530

    Changes in bwhealth cron

--- public/bwhealthmailscript/mail.php
+++ public/bwhealthmailscript/mail.php
@@ -13,7 +13,7 @@ class Cron {
     function __construct() {
         $this->conn = new mysqli('130.211.250.132', 'root', 'bw#digital#2017#cms', 'bw_cms_db') or die($this->conn->connect_errno);
         //$this->conn = new mysqli('localhost', 'root', 'admin', '17novlivecms') or die($this->conn->connect_errno);
-        $this->url='http://bwhealth.businessworld.in/';
+        $this->url='http://bwhealthcareworld.businessworld.in/';
         
     }
 
@@ -86,7 +86,7 @@ class Cron {
         $return_html .= '<head>';
         $return_html .= '<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />';
         $return_html .= '<meta name="viewport" content="width=device-width; initial-scale=1.0; maximum-scale=1.0;">';
-        $return_html .= '<title>BW Health</title>';
+        $return_html .= '<title>BW HealthcareWorld</title>';
         $return_html .= '<style type="text/css">
 
         /* iPad Text Smoother */
@@ -212,7 +212,7 @@ class Cron {
         $return_html .= '<tr><td width="590" height="12"></td></tr>';
         $return_html .= '<tr><td width="590" style="font-size: 22px; color: #2f2f36; text-align: left; font-weight: bold; font-family: Helvetica, Arial, sans-serif; line-height: 30px;"><singleline>Dear&nbsp;'.$name .',</singleline></td></tr>';
         $return_html .= '<tr><td width="590" height="10"></td></tr>';
-        $return_html .= '<tr><td width="590" style="font-size: 14px; color: #696a78; text-align: left; font-weight: normal; font-family: Helvetica, Arial, sans-serif; line-height: 26px;"><singleline>Your article titled <a href="'.$url.'" target="_blank">'.$articletitle.'&nbsp;(click here!)</a> has been published on BW Health website.</singleline></td></tr>';
+        $return_html .= '<tr><td width="590" style="font-size: 14px; color: #696a78; text-align: left; font-weight: normal; font-family: Helvetica, Arial, sans-serif; line-height: 26px;"><singleline>Your article titled <a href="'.$url.'" target="_blank">'.$articletitle.'&nbsp;(click here!)</a> has been published on BW HealthcareWorld website.</singleline></td></tr>';
         $return_html .= '<tr><td width="590" height="30"></td></tr>';
         $return_html .= '<tr><td width="590" style="font-size: 14px; color: #696a78; text-align: left; font-weight: normal; font-family: Helvetica, Arial, sans-serif; line-height: 26px;"><b>What can you do now?</b></td></tr>'
                 . '<tr><td width="590" style="font-size: 14px; color: #696a78; text-align: left; font-weight: normal; font-family: Helvetica, Arial, sans-serif; line-height: 26px;"><a style="text-decoration: none; color: inherit; display: block; width: 180px;" href="'.$url.'" target="_blank"><img src="http://d1s8mqgwixvb29.cloudfront.net/static/images/fb.png" alt="Facebook"/>&nbsp;&nbsp; <span style="float:right">Share it on Facebook</span></a></td></tr>'
